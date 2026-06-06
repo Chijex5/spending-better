cb24fbdc3908e12ef3e2fd27d2a56304710b456f
Chijioke Uzodinma <embroconnect3@gmail.com>
2026-06-06 20:35:17 +0100

    ok

--- Monike/src/services/api.ts
+++ Monike/src/services/api.ts
@@ -1,6 +1,6 @@
 import { Platform } from 'react-native';
 
-const DEFAULT_API_BASE_URL = Platform.OS === 'android' ? 'http://10.0.2.2:8000' : 'http://localhost:8000';
+const DEFAULT_API_BASE_URL = Platform.OS === 'android' ? 'http://192.168.167.58:8000' : 'http://localhost:8000';
 
 export const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL ?? DEFAULT_API_BASE_URL;
 
